7f19cf8f0272347c1f5b74ba7383f79bf1e20fd7
Divyaraj Makwana <divyaraj.makwana9425@gmail.com>
2024-04-04 19:29:18 +0530

    editAllow and url preserve done, when we change url, the old url remains same and added to that new url

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -9,26 +9,32 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./edit-server.component.css']
 })
 export class EditServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number, name: string, status: string };
   serverName = '';
   serverStatus = '';
+  allowEditBool = false;
 
-  constructor(private serversService: ServersService ,private route:ActivatedRoute) { }
+  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     console.log(this.route.snapshot.queryParams);
     console.log(this.route.snapshot.fragment);
 
-    this.route.queryParams.subscribe()
+    this.route.queryParams.subscribe(
+      (queryParams: Params) => {
+        this.allowEditBool = queryParams['allowEdit'] === '1' ? true : false;
+      }
+    )
+    console.log(this.allowEditBool);
     this.route.fragment.subscribe()
-    
+
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
 
   onUpdateServer() {
-    this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.serversService.updateServer(this.server.id, { name: this.serverName, status: this.serverStatus });
   }
 
 }
