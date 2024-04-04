a7a89dfb15b9ca6da7a95f90317a2c8323615d1f
Divyaraj Makwana <divyaraj.makwana9425@gmail.com>
2024-04-04 17:10:34 +0530

    added some comments

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -7,12 +8,18 @@ import { ServersService } from './servers.service';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  public servers: {id: number, name: string, status: string}[] = [];
+  public servers: { id: number, name: string, status: string }[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
   }
 
+  onReload() {
+    // navigate property doesnt know the url, so it will not reload, it will simply redirect to that route
+    // this.router.navigate(['/servers']) // => gives /servers page, but when it is on that page, it will not reload
+    // this.router.navigate(['servers']) // => gives /servers page, but when it is on that page, it will not reload
+    this.router.navigate(['/servers'], { relativeTo: this.route }) // this will tell the navigate property that the button is in this path
+  }
 }
